dcc5c972640a8687a5a4fccd1a3638e734bcb468
vzuluagag12 <144352922+vzuluagag12@users.noreply.github.com>
2024-04-30 15:22:54 -0500

    CentroComercial.java

--- src/CentroComercial.java
+++ src/CentroComercial.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class CentroComercial {
+    private String nombreTienda;
+    private String tipoDeComercio;
+    private double descuento;
+
+    public CentroComercial(String nombreTienda, String tipoDeComercio, double descuento) {
+        this.nombreTienda = nombreTienda;
+        this.tipoDeComercio = tipoDeComercio;
+        this.descuento = descuento;
+    }
+
+    public double calcularDescuento() {
+        if (tipoDeComercio.equalsIgnoreCase("Ropa de niño")) {
+            return descuento * 0.8;
+        } else if (tipoDeComercio.equalsIgnoreCase("Dispositivos Electrónicos")) {
+            return descuento * 0.4;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre de la tienda: " + nombreTienda + "\nTipo de comercio: " + tipoDeComercio + "\nDescuento aplicado: " + calcularDescuento();
     }
 }
