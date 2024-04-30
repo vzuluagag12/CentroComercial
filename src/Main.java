98e30f40d30f4125a635d3a13d6bd6a8a4b82c3f
vzuluagag12 <144352922+vzuluagag12@users.noreply.github.com>
2024-04-30 15:23:42 -0500

    Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Ejemplo de uso
+        Tienda tienda1 = new Tienda("Tienda Interestelar", "Ropa de niño", 100);
+        Tienda tienda2 = new Tienda("Tienda Estelar", "Dispositivos Electrónicos", 200);
+
+        System.out.println(tienda1);
+        System.out.println(tienda2);
+    }
+}
